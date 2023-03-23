a357e855b204662a0c8948076a18132889fe724c
merveegurler <mgurler@gtu.edu.tr>
2023-03-23 20:08:48 +0300

    project1 bugfix

---
+++ build/precache-manifest.464f33971c68881de37ad067b68c9e71.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "0087cfa8ff9a93c12134",
+    "url": "/personal-portfolio/static/css/main.99f756a0.chunk.css"
+  },
+  {
+    "revision": "0087cfa8ff9a93c12134",
+    "url": "/personal-portfolio/static/js/main.28f67cff.chunk.js"
+  },
+  {
+    "revision": "05e37a1631562fd05d07",
+    "url": "/personal-portfolio/static/js/runtime~main.f21b1ced.js"
+  },
+  {
+    "revision": "336c8717f70cd4954b41",
+    "url": "/personal-portfolio/static/js/2.2f57d202.chunk.js"
+  },
+  {
+    "revision": "e369271064879ca58395",
+    "url": "/personal-portfolio/static/js/3.2daa992f.chunk.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/personal-portfolio/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "5139e6642f5bae490ea1e5a576829de0",
+    "url": "/personal-portfolio/static/media/nav-icon1.5139e664.svg"
+  },
+  {
+    "revision": "7967bd7a89e84fd134aea6c07676caa3",
+    "url": "/personal-portfolio/static/media/nav-icon3.7967bd7a.svg"
+  },
+  {
+    "revision": "c484bdd25e0df79484f42e0b7af30601",
+    "url": "/personal-portfolio/static/media/furniture1.c484bdd2.png"
+  },
+  {
+    "revision": "a82d1dfb1c6e65404092e9e146b8220c",
+    "url": "/personal-portfolio/static/media/header-img.a82d1dfb.jpg"
+  },
+  {
+    "revision": "00e4fd36ea329b5eb27534e99ee69743",
+    "url": "/personal-portfolio/static/media/gtu_app.00e4fd36.jpeg"
+  },
+  {
+    "revision": "22a5eeb4fdcae7d959c53bc1736f4c3d",
+    "url": "/personal-portfolio/static/media/pro1-bg.22a5eeb4.jpg"
+  },
+  {
+    "revision": "4ea3794d631bedab7999678f46b0a162",
+    "url": "/personal-portfolio/static/media/CentraNo2-Bold.4ea3794d.ttf"
+  },
+  {
+    "revision": "386f05946a76935fcabf319affff517a",
+    "url": "/personal-portfolio/static/media/CentraNo2-Book.386f0594.ttf"
+  },
+  {
+    "revision": "63911e8535278ced52b8a37e819f6f5d",
+    "url": "/personal-portfolio/static/media/CentraNo2-Medium.63911e85.ttf"
+  },
+  {
+    "revision": "af6022e9a2f97607e8bdd4d378b9d0d7",
+    "url": "/personal-portfolio/static/media/banner-bg.af6022e9.png"
+  },
+  {
+    "revision": "7b06a765e10c3b5cac5324108a5f61a0",
+    "url": "/personal-portfolio/index.html"
+  }
+];
