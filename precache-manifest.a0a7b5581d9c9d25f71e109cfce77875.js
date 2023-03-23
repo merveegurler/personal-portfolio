98d9c7a70478a3aa74c20812ed6031032ab8bd5b
merveegurler <mgurler@gtu.edu.tr>
2023-03-23 14:48:10 +0300

    Updates

---
+++ precache-manifest.a0a7b5581d9c9d25f71e109cfce77875.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "6ad75b4c1f99a22b95d7",
+    "url": "/personal-portfolio/static/css/main.aa22be2e.chunk.css"
+  },
+  {
+    "revision": "6ad75b4c1f99a22b95d7",
+    "url": "/personal-portfolio/static/js/main.4c8506b8.chunk.js"
+  },
+  {
+    "revision": "d04cfbfe8a2c44ceedf6",
+    "url": "/personal-portfolio/static/js/runtime~main.177ebd2c.js"
+  },
+  {
+    "revision": "c5d281188534cf7272c1",
+    "url": "/personal-portfolio/static/js/2.71108ec2.chunk.js"
+  },
+  {
+    "revision": "c2c22b812063430edb1a",
+    "url": "/personal-portfolio/static/js/3.027b0bf5.chunk.js"
+  },
+  {
+    "revision": "c484bdd25e0df79484f42e0b7af30601",
+    "url": "/personal-portfolio/static/media/furniture1.c484bdd2.png"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/personal-portfolio/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "5139e6642f5bae490ea1e5a576829de0",
+    "url": "/personal-portfolio/static/media/nav-icon1.5139e664.svg"
+  },
+  {
+    "revision": "7967bd7a89e84fd134aea6c07676caa3",
+    "url": "/personal-portfolio/static/media/nav-icon3.7967bd7a.svg"
+  },
+  {
+    "revision": "a82d1dfb1c6e65404092e9e146b8220c",
+    "url": "/personal-portfolio/static/media/header-img.a82d1dfb.jpg"
+  },
+  {
+    "revision": "4ea3794d631bedab7999678f46b0a162",
+    "url": "/personal-portfolio/static/media/CentraNo2-Bold.4ea3794d.ttf"
+  },
+  {
+    "revision": "63911e8535278ced52b8a37e819f6f5d",
+    "url": "/personal-portfolio/static/media/CentraNo2-Medium.63911e85.ttf"
+  },
+  {
+    "revision": "386f05946a76935fcabf319affff517a",
+    "url": "/personal-portfolio/static/media/CentraNo2-Book.386f0594.ttf"
+  },
+  {
+    "revision": "22a5eeb4fdcae7d959c53bc1736f4c3d",
+    "url": "/personal-portfolio/static/media/pro1-bg.22a5eeb4.jpg"
+  },
+  {
+    "revision": "af6022e9a2f97607e8bdd4d378b9d0d7",
+    "url": "/personal-portfolio/static/media/banner-bg.af6022e9.png"
+  },
+  {
+    "revision": "d6c694bfe72361fdd4633c47c0a3000f",
+    "url": "/personal-portfolio/index.html"
+  }
+];
